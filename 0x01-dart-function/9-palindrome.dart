fa992c77e990ef08250ff785284a577e82c54545
cpmoore0317 <cpmoore317@gmail.com>
2024-10-07 17:19:57 -0500

    Task 9 v2

--- 0x01-dart-function/9-palindrome.dart
+++ 0x01-dart-function/9-palindrome.dart
@@ -1,3 +1,10 @@
 bool isPalindrome(String s) {
+  // Check if the length of the string is less than 3
+  if (s.length < 3) {
+    return false;
+  }
   
+  // Reverse the string and compare it with the original
+  String reversed = s.split('').reversed.join('');
+  return s == reversed;
 }
