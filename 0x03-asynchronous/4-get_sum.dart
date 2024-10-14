45c4b24f463202784be161060fd0e9c75e53549b
cpmoore0317 <cpmoore317@gmail.com>
2024-10-14 15:48:13 -0500

    Task 4 v4

--- 0x03-asynchronous/4-get_sum.dart
+++ 0x03-asynchronous/4-get_sum.dart
@@ -10,13 +10,13 @@ Future<double> calculateTotal() async {
     // Fetch user's orders based on their ID
     String ordersData = await fetchUserOrders(user['id']);
     
-    // Check if ordersData is null or empty
-    if (ordersData == null) {
+    // Check if ordersData is null
+    if (ordersData == null || ordersData == 'null') {
       throw 'User orders not found';
     }
 
     List<dynamic> orders = jsonDecode(ordersData);
-
+    
     // Calculate total price of all orders
     double total = 0;
     for (String order in orders) {
